592f9c0b88257289ef798b169e808425957c4ff5
Emanuel Dobra <ema.dobra@gmail.com>
2020-11-14 11:56:23 -0500

    Finished A2P3

--- Assignment2P3_Emanuel_Dobra.cpp
+++ Assignment2P3_Emanuel_Dobra.cpp
@@ -6,19 +6,18 @@ using namespace std;
 
 /*****************************************************************
 Name: Emanuel Dobra                          Assignment #2 Phase 3
-Program: Handles multiple guest charges at a hotel. Phase 1 will
- work for one guest only. Phase 2 will distinguish between room
- types and output price and details accordingly. Phase 3 
+Program: Handles multiple guest  charges at a  hotel. Phase 1 will
+ work for one guest  only. Phase 2 will distinguish  between  room
+ types and output price  and details accordingly. Phase 3 will let
+ multiple guests  be registered  until user decides to stop input.
 *****************************************************************/
 int main()
 {
-    // Variable declarations with defaults
-    string guestName = "Not set";
-    char roomType = 'G';
-    int numOfDays = 1;
-    double roomCharge = 125.00;
-    double roomRate = 0;
-    string roomName = "Not set";
+    // Variable declarations
+    string guestName, roomName;
+    char roomType;
+    int numOfDays;
+    double roomCharge, roomRate;
 
     // Open output file
     ofstream fout("charges.dat");
@@ -40,51 +39,85 @@ int main()
         << right << setw(15) << "Room Charge" << endl;
 
     // User Input
-    cout << "What is your name: ";
-    getline(cin, guestName);
-    cout << "What room type would you like (G, P or L): ";
-    cin >> roomType;
-    cout << "How many days would you like to stay: ";
-    cin >> numOfDays;
+    cout << "Input the first guest's name (or ^Z to quit): ";
 
-    // Determine room name and rate
-    if (roomType == 'G') {
-        roomRate = 125.00;
-        roomName = "Garden View";
-    }
-    else if (roomType == 'P') {
-        roomRate = 145.00;
-        roomName = "Pool View";
-    }
-    else if (roomType == 'L') {
-        roomRate = 180.00;
-        roomName = "Lake View";
-    }
-    else {
-        cout << "Invalid Room Type Input" << endl;
-        cout << "Program has been terminated" << endl; 
-        system("pause");
-        exit(-1);
-    }
+    /* I tried using the following:
+    * while (!cin.eof()) { }
+    * However, it would always cause an extra loop which would
+    *  add another blank guest with the last used roomType, 
+    *  roomRate and numOfDays input. The only way I found of 
+    *  fixing this without adding more arguments to the while 
+    *  loop is by using the getline as the while loop argument
+    *  instead of !cin.eof, which will stop the program before
+    *  it loops an extra time. I hope this is okay.
+    * Example of what output used to look like:
+    * Guest Name          Room Type      # Days        Room Charge
+    * Sahim               Garden View    24                3000.00
+    * Sarah               Pool View      12                1740.00
+    * Corinth             Lake View      4                  720.00
+    * Sonder              Pool View      7                 1015.00
+    *                     Pool View      7                 1015.00 <-- This is the line I wanted to get rid of
+    */
+    while (getline(cin, guestName)) {
+        cout << "What room type would you like (G, P or L): ";
+        cin >> roomType;
+        cout << "How many days would you like to stay: ";
+        cin >> numOfDays;
 
-    // Calculate charge
-    roomCharge = numOfDays * roomCharge;
+        // Determine room name and rate
+        if (roomType == 'G') {
+            roomRate = 125.00;
+            roomName = "Garden View";
+        }
+        else if (roomType == 'P') {
+            roomRate = 145.00;
+            roomName = "Pool View";
+        }
+        else if (roomType == 'L') {
+            roomRate = 180.00;
+            roomName = "Lake View";
+        }
+        else {
+            cout << "Invalid Room Type Input" << endl;
+            cout << "Program has been terminated" << endl;
+            system("pause");
+            exit(-1);
+        }
 
-    // Output info to file
-    fout << left << setw(20) << guestName
-        << left << setw(15) << roomName
-        << left << setw(10) << numOfDays
-        << right << setw(15) << roomCharge << endl;
+        // Calculate charge
+        roomCharge = numOfDays * roomRate;
+
+        // Output info to file
+        fout << left << setw(20) << guestName
+            << left << setw(15) << roomName
+            << left << setw(10) << numOfDays
+            << right << setw(15) << roomCharge << endl;
+        cout << "Please input the next guest's name (or ^Z to quit): ";
+        cin.ignore(80, '\n');
+    }
 
     system("type charges.dat");
     system("pause");
 }
 /* Output
-What is your name: Emanuel Dobra
+Input the first guest's name (or ^Z to quit): Sahim
+What room type would you like (G, P or L): G
+How many days would you like to stay: 24
+Please input the next guest's name (or ^Z to quit): Sarah
 What room type would you like (G, P or L): P
-How many days would you like to stay: 17
+How many days would you like to stay: 12
+Please input the next guest's name (or ^Z to quit): Corinth
+What room type would you like (G, P or L): L
+How many days would you like to stay: 4
+Please input the next guest's name (or ^Z to quit): Sonder
+What room type would you like (G, P or L): P
+How many days would you like to stay: 7
+Please input the next guest's name (or ^Z to quit): ^Z
                CPA Resort Hotel Room Charge
 Guest Name          Room Type      # Days        Room Charge
-Emanuel Dobra       Pool View      17                2125.00
+Sahim               Garden View    24                3000.00
+Sarah               Pool View      12                1740.00
+Corinth             Lake View      4                  720.00
+Sonder              Pool View      7                 1015.00
 Press any key to continue . . .
 */
